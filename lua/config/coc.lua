f10c8573527720325f78564a9548a18475e4c2d1
seiya <20365512+seiyab@users.noreply.github.com>
2025-07-28 13:47:02 +0000

    tab to accept autocomplete

--- lua/config/coc.lua
+++ lua/config/coc.lua
@@ -30,7 +30,7 @@ local opts = {silent = true, noremap = true, expr = true, replace_keycodes = fal
 
 -- Make <CR> to accept selected completion item or notify coc.nvim to format
 -- <C-g>u breaks current undo, please make your own choice
--- keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
+keyset("i", "<TAB>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<TAB>\<c-r>=coc#on_enter()\<TAB>"]], opts)
 
 -- Use <c-j> to trigger snippets
 -- keyset("i", "<c-j>", "<Plug>(coc-snippets-expand-jump)")
